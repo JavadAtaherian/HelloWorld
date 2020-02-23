93006d6beb6b9c619c3aa009f787c47ac4d567a1
JavadAtaherian <61148380+JavadAtaherian@users.noreply.github.com>
2020-02-23 13:36:23 +0330

    add file

---
+++ Main.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+        int thenumber = scanner.nextInt();
+
+        if (thenumber>= -128 && thenumber<=127) {
+            System.out.println("byte");
+            
+        }
+        
+        if (thenumber>= -32768 && thenumber<=32768) {
+            System.out.println("short");
+        }
+
+        if (thenumber>= -2147483648 && thenumber<= 2147483647) {
+
+            System.out.println("int");
+        }
+
+        
+            System.out.println("long");
+        
+
+
+        
+            
+        
+		
+        
+        
+    }
+
+
+    
+
+
+
+
+
+}
